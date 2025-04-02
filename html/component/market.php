9dcf87f166da301fd9e2e6c70885fc06d61a175f
g_matsuo <gmatsuo@pricingdata.co.jp>
2025-04-02 19:02:08 +0900

    UI要素追加対応中

--- html/component/market.php
+++ html/component/market.php
@@ -1,7 +1,7 @@
 <div class="page_market_filed">
     <div class="page_market_blade responce_flex">
         <div class="page_market_late_container">
-            <div class="page_market_late_flex_group no_responce_flex">
+            <div class="page_market_late_flex_group responce_flex">
 
                 <div class="page_market_late_box position_center">
                     <div class="page_market_country_info">
@@ -37,16 +37,41 @@
         </div>
 
         <div class="page_market_gold_late_container">
-            <div class="page_market_gold_late_box no_responce_flex">
-                <div class="page_market_gold_label"></div>
-                <div class="page_market_gold_price"></div>
-            </div>
-            <div class="gold_late_box no_responce_flex">
-                <div class="page_market_gold_label"></div>
-                <div class="page_market_gold_price"></div>
+            <div class="page_market_gold_late_group">
+                <div class="page_market_gold_late_box no_responce_flex">
+                    <div class="page_market_gold_label"><div class="position_center">24金</div></div>
+                    <div class="page_market_gold_price">￥ 1,000,000.3</div>
+                </div>
+                <div class="page_market_gold_late_box no_responce_flex">
+                    <div class="page_market_gold_label"><div class="position_center">18金</div></div>
+                    <div class="page_market_gold_price">￥ 1,000,000.3</div>
+                </div>
             </div>
             <div class="page_market_late_container_date">test_date</div>
         </div>
-
     </div>
+
+    <div class="page_market_blade responce_flex">
+        <div class="page_market_label_block no_responce_flex">
+            <img src="/image/ic_search.svg">
+            <div class="page_market_search_caption position_center">商品の検索</div>
+        </div>
+        <div class="page_market_input_block">
+            <div class="search_categorys">
+                <label><input type="radio" name="category" category_id="null">全て</label>
+                <label><input type="radio" name="category" category_id="1"   >時計</label>
+                <label><input type="radio" name="category" category_id="2"   >宝石</label>
+                <label><input type="radio" name="category" category_id="3"   >ブランド</label>
+            </div>
+            <div class="no_responce_flex">
+                <input class="page_market_text_input" type="text" placeholder="フリーワード">
+                <div class="page_market_label_organize">
+                    <label><input type="radio" name="search_type" query_select="exact">完全一致</label>
+                    <label><input type="radio" name="search_type" query_select="partial">部分一致</label>
+                </div>
+                <div class="button search_button position_center">検索</div>
+            </div>
+        </div>
+    </div>
+
 </div>
