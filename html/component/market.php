bdb3c9d525393a9836508a4090261accd7ddd0fd
g_matsuo <gmatsuo@pricingdata.co.jp>
2025-04-03 13:34:04 +0900

    一旦おくらいりー（笑）

--- html/component/market.php
+++ html/component/market.php
@@ -58,20 +58,43 @@
         </div>
         <div class="page_market_input_block">
             <div class="search_categorys">
-                <label><input type="radio" name="category" category_id="null">全て</label>
-                <label><input type="radio" name="category" category_id="1"   >時計</label>
-                <label><input type="radio" name="category" category_id="2"   >宝石</label>
-                <label><input type="radio" name="category" category_id="3"   >ブランド</label>
+                <label><input type="radio" name="category" category_id="null" checked>全て</label>
+                <label><input type="radio" name="category" category_id="1"           >時計</label>
+                <label><input type="radio" name="category" category_id="2"           >宝石</label>
+                <label><input type="radio" name="category" category_id="3"           >ブランド</label>
             </div>
             <div class="no_responce_flex">
                 <input class="page_market_text_input" type="text" placeholder="フリーワード">
                 <div class="page_market_label_organize">
-                    <label><input type="radio" name="search_type" query_select="exact">完全一致</label>
-                    <label><input type="radio" name="search_type" query_select="partial">部分一致</label>
+                    <label><input type="radio" name="search_type" query_select="exact" checked>完全一致</label>
+                    <label><input type="radio" name="search_type" query_select="partial"      >部分一致</label>
                 </div>
-                <div class="button search_button position_center">検索</div>
             </div>
         </div>
+        <div class="button search_button position_center page_search_button">検索</div>
+    </div>
+
+    <div class="page_market_blade responce_flex">
+        <div class="page_market_label_block no_responce_flex">
+            <img src="/image/ic_filter.svg">
+            <div class="page_market_search_caption position_center">絞り込み検索</div>
+        </div>
+        <div class="page_market_input_block">
+            <div class="search_categorys">
+                <label><input type="radio" name="category" category_id="null" checked>全て</label>
+                <label><input type="radio" name="category" category_id="1"           >時計</label>
+                <label><input type="radio" name="category" category_id="2"           >宝石</label>
+                <label><input type="radio" name="category" category_id="3"           >ブランド</label>
+            </div>
+            <div class="no_responce_flex">
+                <input class="page_market_text_input" type="text" placeholder="フリーワード">
+                <div class="page_market_label_organize">
+                    <label><input type="radio" name="search_type" query_select="exact" checked>完全一致</label>
+                    <label><input type="radio" name="search_type" query_select="partial"      >部分一致</label>
+                </div>
+            </div>
+        </div>
+        <div class="button search_button position_center page_search_button">検索</div>
     </div>
 
 </div>
